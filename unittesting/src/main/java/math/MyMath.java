77274255381f278e6b410c2a2dc29b289b9eb426
AirlineDog <t8190042@aueb.gr>
2022-05-17 15:55:11 +0100

    Add isPrime and its test cases

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -1,6 +1,11 @@
 package math;
 
 public class MyMath {
+	/**
+	 * Calculate the factorial of n
+	 * @param n
+	 * @return the factorial of n
+	 */
 	public int factorial(int n) {
 		if (n < 0 || n > 12) {
 			throw new IllegalArgumentException();
@@ -10,5 +15,24 @@ public class MyMath {
 		}
 
 		return n * factorial(n - 1);
+	}
+
+	/**
+	 * Check if n is prime
+	 * @param n
+	 * @return true if n is prime else false
+	 */
+	public boolean isPrime(int n) {
+		if (n < 2) {
+			throw new IllegalArgumentException();
+		}
+		int i = 2;
+		while (i * i < n) {
+			if (n % i == 0) {
+				return false;
+			}
+			i++;
+		}
+		return true;
 	}
 }
--- unittesting/src/test/java/math/MyMathTest.java
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -41,4 +41,34 @@ public class MyMathTest {
 	public void test_factorial_more_than_12() {
 		mm.factorial(13);
 	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void test_isPrime_negative() {
+		mm.isPrime(-1);
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void test_isPrime_0() {
+		mm.isPrime(0);
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void test_isPrime_1() {
+		mm.isPrime(1);
+	}
+
+	@Test
+	public void test_isPrime_2() {
+		Assert.assertTrue(mm.isPrime(2));
+	}
+
+	@Test
+	public void test_isPrime_false_positive() {
+		Assert.assertFalse(mm.isPrime(74));
+	}
+
+	@Test
+	public void test_isPrime_true_positive() {
+		Assert.assertTrue(mm.isPrime(241));
+	}
 }
