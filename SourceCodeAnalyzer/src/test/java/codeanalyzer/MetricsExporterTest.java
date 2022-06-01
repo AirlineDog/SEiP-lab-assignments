72a8362277c073ea3ce73b37a88d4bb03418cc48
AirlineDog <t8190042@aueb.gr>
2022-06-01 19:14:27 +0100

    Refactor Tests

--- SourceCodeAnalyzer/src/test/java/codeanalyzer/MetricsExporterTest.java
+++ SourceCodeAnalyzer/src/test/java/codeanalyzer/MetricsExporterTest.java
@@ -1,5 +1,9 @@
 package codeanalyzer;
 
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,54 +11,47 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
-import codeanalyzer.MetricsExporter;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doCallRealMethod;
-
 public class MetricsExporterTest {
-	
-	MetricsExporter mex = new MetricsExporter();
-	
+	private MetricsExporterFactory fct = new MetricsExporterFactory();
+
 	@Test
 	public void testWriteCsv() {
+		MetricsExporter mex = fct.createMetricsExporter("csv");
 		// create the metrics content
 		Map<String, Integer> metrics = new HashMap<>();
 		metrics.put("loc",30);
 		metrics.put("nom",5);
 		metrics.put("noc",2);
-		
+
 		// generate and write the output file
 		String outputFilepath = "src/test/resources/output_metrics";
-		mex.writeFile("csv", metrics, outputFilepath);
-		
+		mex.writeFile(metrics, outputFilepath);
+
 		// evaluate that the file exists
 		File outputFile = new File(outputFilepath + ".csv");
 		Assert.assertTrue(outputFile.exists());
-		
+
 		// delete the generated file
 		outputFile.delete();
 	}
-	
+
 	@Test
 	public void testWriteJson() {
-		MetricsExporter mockedExporter = mock(MetricsExporter.class);
+		MetricsExporter mockedExporter = mock(JsonMetricsExporter.class);
 		// create an empty metrics content
 		Map<String, Integer> metrics = new HashMap<>();
 		String outputFilepath = "whatever-path";
-		
+
 		//this is a demo of how a mocked object can call a real method (partial mocking)
-		doCallRealMethod().when(mockedExporter).writeFile("json", metrics, outputFilepath);
-		mockedExporter.writeFile("json", metrics, outputFilepath);
+		doCallRealMethod().when(mockedExporter).writeFile(metrics, outputFilepath);
+		mockedExporter.writeFile(metrics, outputFilepath);
 		//just verify that the method was executed/called
-		verify(mockedExporter).writeFile("json", metrics, outputFilepath);
+		verify(mockedExporter).writeFile(metrics, outputFilepath);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testWriteFileWithUknownFIleType() {
-		mex.writeFile("non-existing-type", null, null);
+		MetricsExporter mex = fct.createMetricsExporter("non-existing-type");
 	}
 
 }
--- SourceCodeAnalyzer/src/test/java/codeanalyzer/SourceCodeAnalyzerTest.java
+++ SourceCodeAnalyzer/src/test/java/codeanalyzer/SourceCodeAnalyzerTest.java
@@ -1,56 +1,55 @@
 package codeanalyzer;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import codeanalyzer.SourceCodeAnalyzer;
-
-import static org.junit.Assert.*;
 
 public class SourceCodeAnalyzerTest {
 	private static List<String> testSourceCode;
 	private final static String TYPE_REGEX = "regex";
 	private final static String TYPE_STRCOMP = "strcomp";
+	private final static String TYPE_LOCAL = "local";
 	private final static String TEST_CLASS = "src/test/resources/TestClass.java";
-	private SourceCodeAnalyzer sca = new SourceCodeAnalyzer("local");
-	
-	
+	private SourceCodeAnalyzerFactory fct = new SourceCodeAnalyzerFactory();
+
+
 	@Test
 	public void testCalculateRegexLOC() throws IOException {
-		assertEquals(21, sca.calculateLOC(TEST_CLASS, TYPE_REGEX));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_REGEX, TYPE_LOCAL);
+		assertEquals(21, sca.calculateLOC(TEST_CLASS));
 	}
-	
+
 	@Test
 	public void testCalculateStrCompLOC() throws IOException {
-		assertEquals(7, sca.calculateLOC(TEST_CLASS, TYPE_STRCOMP));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_STRCOMP, TYPE_LOCAL);
+		assertEquals(7, sca.calculateLOC(TEST_CLASS));
 	}
-	
+
 	@Test
 	public void testCalculateRegexNOM() throws IOException {
-		assertEquals(3, sca.calculateNOM(TEST_CLASS, TYPE_REGEX));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_REGEX, TYPE_LOCAL);
+		assertEquals(3, sca.calculateNOM(TEST_CLASS));
 	}
-	
+
 	@Test
 	public void testCalculateStrCompNOM() throws IOException {
-		assertEquals(3, sca.calculateNOM(TEST_CLASS, TYPE_STRCOMP));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_STRCOMP, TYPE_LOCAL);
+		assertEquals(3, sca.calculateNOM(TEST_CLASS));
 	}
-	
+
 	@Test
 	public void testCalculateRegexNOC() throws IOException {
-		assertEquals(3, sca.calculateNOC(TEST_CLASS, TYPE_REGEX));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_REGEX, TYPE_LOCAL);
+		assertEquals(3, sca.calculateNOC(TEST_CLASS));
 	}
-	
+
 	@Test
 	public void testCalculateStrCompNOC() throws IOException {
-		assertEquals(3, sca.calculateNOC(TEST_CLASS, TYPE_STRCOMP));
+		SourceCodeAnalyzer sca = fct.createSourceCodeAnalyzer(TYPE_STRCOMP, TYPE_LOCAL);
+		assertEquals(3, sca.calculateNOC(TEST_CLASS));
 	}
-	
+
 }
