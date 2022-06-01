466380c20937da428ffddc0587377aa7cefd93f0
AirlineDog <t8190042@aueb.gr>
2022-06-01 18:03:58 +0100

    Add interface for MetricsExporters

---
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/JsonMetricsExporter.java
@@ -0,0 +1,13 @@
+package codeanalyzer;
+
+import java.util.Map;
+
+public class JsonMetricsExporter implements MetricsExporter {
+
+	@Override
+	public void writeFile(Map<String, Integer> metrics, String filepath) {
+		// Functionality not implemented yet
+		// No need to implement it for the assignment
+	}
+
+}
