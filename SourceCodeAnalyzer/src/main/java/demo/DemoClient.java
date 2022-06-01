db620b827a743aa08eeee4b95239c8037bb9cba4
AirlineDog <t8190042@aueb.gr>
2022-06-01 18:36:46 +0100

    Refactor main

--- SourceCodeAnalyzer/src/main/java/demo/DemoClient.java
+++ SourceCodeAnalyzer/src/main/java/demo/DemoClient.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import codeanalyzer.CsvMetricsExporter;
-import codeanalyzer.JsonMetricsExporter;
 import codeanalyzer.MetricsExporter;
-import codeanalyzer.RegexSourceCodeAnalyzer;
+import codeanalyzer.MetricsExporterFactory;
 import codeanalyzer.SourceCodeAnalyzer;
-import codeanalyzer.StrcompSourceCodeAnalyzer;
+import codeanalyzer.SourceCodeAnalyzerFactory;
 
 public class DemoClient {
 
@@ -30,17 +28,10 @@ public class DemoClient {
 			System.out.println("Incorrect number of arguments.");
 			System.exit(1);
 		}
-		SourceCodeAnalyzer analyzer = null;
-		if (sourceCodeAnalyzerType.equals("regex")) {
-			analyzer = new RegexSourceCodeAnalyzer(sourceFileLocation);
+		SourceCodeAnalyzerFactory srcfct = new SourceCodeAnalyzerFactory();
 
-		}else if (sourceCodeAnalyzerType.equals("strcomp")) {
-			analyzer = new StrcompSourceCodeAnalyzer(sourceFileLocation);
+		SourceCodeAnalyzer analyzer = srcfct.createSourceCodeAnalyzer(sourceFileLocation);
 
-		}else {
-			System.out.println("Incorrect argument 2 [regex|strcomp].");
-			System.exit(1);
-		}
 		int loc = analyzer.calculateLOC(filepath);
 		int nom = analyzer.calculateNOM(filepath);
 		int noc = analyzer.calculateNOC(filepath);
@@ -50,16 +41,8 @@ public class DemoClient {
 		metrics.put("nom",nom);
 		metrics.put("noc",noc);
 
-
-		MetricsExporter exporter = null;
-		if (outputFileType.equals("csv")) {
-			exporter = new CsvMetricsExporter();
-		} else if (outputFileType.equals("json")) {
-			exporter = new JsonMetricsExporter();
-		} else {
-			System.out.println("Incorrect argument 4 [csv|json].");
-			System.exit(1);
-		}
+		MetricsExporterFactory expfct = new MetricsExporterFactory();
+		MetricsExporter exporter = expfct.createMetricsExporter(outputFileType);
 		exporter.writeFile(metrics, outputFilePath);
 
 	}
