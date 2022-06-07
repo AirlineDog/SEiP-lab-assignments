53145309614f5b712cf4137e256c5ef250777eb5
AirlineDog <t8190042@aueb.gr>
2022-06-07 09:41:56 +0100

    Remove redundant factorial test cases #7

--- unittesting/src/test/java/math/MyMathTest.java
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -12,31 +12,6 @@ public class MyMathTest {
 		mm.factorial(-1);
 	}
 
-	@Test
-	public void test_factorial_zero() {
-		Assert.assertEquals(1, mm.factorial(0));
-	}
-
-	@Test
-	public void test_factorial_one() {
-		Assert.assertEquals(1, mm.factorial(1));
-	}
-
-	@Test
-	public void test_factorial_two() {
-		Assert.assertEquals(2, mm.factorial(2));
-	}
-
-	@Test
-	public void test_factorial_less_than_12() {
-		Assert.assertEquals(720, mm.factorial(6));
-	}
-
-	@Test
-	public void test_factorial_12() {
-		Assert.assertEquals(479001600, mm.factorial(12));
-	}
-
 	@Test (expected = IllegalArgumentException.class)
 	public void test_factorial_more_than_12() {
 		mm.factorial(13);
